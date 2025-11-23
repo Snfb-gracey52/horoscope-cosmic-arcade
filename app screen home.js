dfea0f8dd1ad0194edb0de1834a2ca1ecb9c9d9a
Snfb-gracey52 <mgthomas623@icloud.com>
2025-11-23 12:29:04 -0500

    Create app screen home.js

---
+++ app screen home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={{ padding: 20 }}>
+      <Text style={{ fontSize: 26 }}>Cosmic F Arcade</Text>
+      <Text>Step into your cosmic playground.</Text>
+    </View>
+  );
+}
